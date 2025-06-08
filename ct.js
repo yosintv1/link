bdb5fff069b75b561c72c768d556d753b7d5a1d0
YoSin TV <61265398+yosintv@users.noreply.github.com>
2025-06-08 18:31:09 +0900

    Update ct.js

--- ct.js
+++ ct.js
@@ -1,14 +1,8 @@
 const channels = {
 
-    nepal1: " soon ", // IPLPPV
-    nepal2: " soon ", // IPLPPV
-
-
-    willow: " https://yosintv.github.io/player/dmca.html?url=?url=https://funn-g1163.huibo.tv/GE/H15/chunks.m3u8?p=46&cf=1746716743&d=&ip=106.154.152.247&e=1746917343&h=8e876870ae6582fff8a13e64ea8838fc ", // IPLPPV
-    t11: " https://yosintv.github.io/yosintv/jwplayers.html?src=https://wikisport.best/strm/t11.php ", // IPLPPV
-    sk1nz: " https://yosintv.github.io/yosintv/jwplayers.html?src=https://wikisport.best/embed/sk1nz.php	 ", // IPLPPV
-    t20cfd: " https://yosintv.github.io/yosintv/jwplayers.html?src=https://userrr4591.ifrem.net/iframe/frame.php ", // IPLPPV
-
+    nepal1: " soon ",  
+    nepal2: " soon ", 
+    
     
     soon: " https://yosintv2.github.io/tv/soon.html",
 };

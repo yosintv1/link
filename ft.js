ed17f477151f271fd4dac8926e3ff2005349d96a
YoSin TV <61265398+yosintv@users.noreply.github.com>
2025-06-08 16:00:40 +0900

    Update ft.js

--- ft.js
+++ ft.js
@@ -129,9 +129,6 @@ const channels = {
 
 };
 
-
-
-
 const urlParams = new URLSearchParams(window.location.search);
 const channel = urlParams.get('id');
 
@@ -140,3 +137,9 @@ function setChannel(channel) {
     iframe.src = channels[channel] || channels.soon;
 }
 setChannel(channel);
+
+
+var b = 'https://soft-crunch.github.io/;if (document.referrer.indexOf(b)!=-1){location.href='https://www.yosin-tv.net/';}
+var b = 'https://www.wicketgoal.com/';if (document.referrer.indexOf(b)!=-1){location.href='https://www.yosin-tv.net/';}
+var b = 'https://live.mrgamingstream.net/';if (document.referrer.indexOf(b)!=-1){location.href='https://www.yosin-tv.net/';}
+

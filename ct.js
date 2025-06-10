e805f457d648e2f2a7f561845fbc94db7879bac0
YoSin TV <61265398+yosintv@users.noreply.github.com>
2025-06-11 02:03:22 +0900

    Update ct.js

--- ct.js
+++ ct.js
@@ -7,7 +7,7 @@ const channels = {
 
     
     
-    nepal1: " https://yosintv.github.io/player/player3.html?url=https://1a-1791.com/live/kd32s8ur/slot-17/2ekg-9ru2_1080p/chunklist.m3u8 ",  
+    nepal1: " https://yosintv.github.io/player/player3.html?url=https://1a-1791.com/live/b9exd20e/slot-31/ad06-advv_1080p/chunklist.m3u8 ",  
     nepal2: "  https://linnk.pages.dev/cricket?id=nepal1  ", 
     nepal3: "  https://linnk.pages.dev/cricket?id=nepal1  ", 
     

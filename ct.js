5368570bbb1b567d4565d7d7c202666cfc28e764
YoSin TV <61265398+yosintv@users.noreply.github.com>
2025-06-17 00:03:26 +0900

    Update ct.js

--- ct.js
+++ ct.js
@@ -7,7 +7,7 @@ const channels = {
 
     
     
-    nepal1: " https://yosintv.github.io/player/dmca.html?url=https://fv1163.huibo.tv/GM/H208/chunks.m3u8?wsSecret=906f0f25bf9aab6dab29fadfdfbe0963&wsTime=1750086191  ",  
+    nepal1: " https://yosintv.github.io/player/dmca.html?url=https://fv1163.huibo.tv/GS/H3105/chunks.m3u8?wsSecret=5597158534cbf88dad9316d2ccc9079d&wsTime=1750086783  ",  
     nepal2: " /cricket?id=nepal1  ", 
     
 

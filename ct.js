0ae2e1e506cc95c170834efafa37398256ed0cf9
YoSin TV <61265398+yosintv@users.noreply.github.com>
2025-06-17 23:27:26 +0900

    Update ct.js

--- ct.js
+++ ct.js
@@ -2,8 +2,8 @@ const channels = {
 
     
 
-nepal1: " https://yosintv.github.io/player/dmca.html?url=https://fv1163.huibo.tv/GPA/H914/chunks.m3u8?wsSecret=ea888d32266b00247ec62b85f3664583&wsTime=1750170905",  
-nepal2: " hhttps://nirutv.online/link.html?id=nepal  ", 
+nepal1: " https://nirutv.online/link.html?id=nepal  ",  
+nepal2: " https://nirutv.online/link.html?id=nepal  ", 
 nepal3: " https://sportslivehub01.live/en-BD/stream/19001984 ", 
 
 };

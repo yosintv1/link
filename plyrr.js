4d6ff9e1039ed1b30aa1a01932d182276094f677
YoSin TV <61265398+yosintv@users.noreply.github.com>
2025-06-18 00:18:28 +0900

    Create plyrr.js

---
+++ plyrr.js
@@ -0,0 +1,14 @@
+    const streamMap = {
+      "sky": {
+        url: "https://nijineh-lalajo.hf.space/proxy?url=https://dokko1new.newkso.ru/dokko1/premium65/mono.m3u8",
+        poster: "https://i.ibb.co/MyvXYC4Z/IMG-20250610-214129.jpg"
+      },
+      "f1": {
+        url: "https://d2sshbxprtwbjg.cloudfront.net/out/v1/5a00690119224df688a7c3d2e9c7022f/index.m3u8",
+        poster: "https://i.ibb.co/0RjkVHK0/IMG-20250614-213759-410.jpg"
+      },
+      "match2": {
+        url: "https://1a-1791.com/live/t34ch9px/slot-35/euf9-qgmb_1080p/chunklist.m3u8",
+        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3jDvDkFrL6QvUm34ahcxcEIRQbMHYChERAA&s"
+      }
+    };

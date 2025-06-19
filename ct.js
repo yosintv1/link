c413132179403169913bd6fced7c595777974ee8
YoSin TV <61265398+yosintv@users.noreply.github.com>
2025-06-20 01:53:25 +0900

    Update ct.js

--- ct.js
+++ ct.js
@@ -2,8 +2,8 @@ const channels = {
 
     
 
-nepal1: " https://yosintv.github.io/player/player3.html?url=https://back.liveboxpro.com/proxy_this/1251.m3u8 ", 
-nepal2: " https://yosintv.github.io/yosintv/jwplayers.html?src=https://nirutv.online/link.html?id=nepal8 ", 
+nepal2: " https://yosintv.github.io/player/player3.html?url=https://back.liveboxpro.com/proxy_this/1251.m3u8 ", 
+nepal1: " https://yosintv.github.io/yosintv/jwplayers.html?src=https://nirutv.online/link.html?id=nepal8 ", 
 nepal3: " https://bintvfunplayernoads.netlify.app/player.html?src=https://polite-capybara-15e90e.netlify.app/player.html?id=willowextra ", 
 
 };

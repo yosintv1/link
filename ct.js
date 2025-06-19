aeb1daa3bdc773bd7541f70f0b44627b8b06b45e
YoSin TV <61265398+yosintv@users.noreply.github.com>
2025-06-19 23:44:41 +0900

    Update ct.js

--- ct.js
+++ ct.js
@@ -2,7 +2,7 @@ const channels = {
 
     
 
-nepal1: " https://yosintv2.github.io/bkp/plyrr.html?url=https://rumble.com/live-hls-dvr/6stsqx/playlist.m3u8 ", 
+nepal1: " https://yosintv.github.io/yosintv/jwplayers.html?src=https://nirutv.online/link.html?id=nepal ", 
 nepal2: " https://yosintv.github.io/yosintv/jwplayers.html?src=https://nirutv.online/link.html?id=nepal ", 
 nepal3: " https://bintvfunplayernoads.netlify.app/player.html?src=https://polite-capybara-15e90e.netlify.app/player.html?id=willowextra ", 
 

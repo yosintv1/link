fda6823b2cad9ce152338f7d185702ba5e0d4fc9
YoSin TV <61265398+yosintv@users.noreply.github.com>
2025-06-24 00:06:27 +0900

    Update plyrr.js

--- plyrr.js
+++ plyrr.js
@@ -1,6 +1,6 @@
     const streamMap = {
       "sky": {
-        fox: " https://uugg.hatsune86.space/lchd-foxsports501.m3u8 ",
+        fox: " https://tataplay.slivcdn.com//hls/live/2011747/TEN1HD/master.m3u8 ",
         poster: " https://images.fancode.com/skillup-uploads/cms-media/127628_5263_SCO_NEP_fc-Web.png?hash=eyJrZXkiOiJza2lsbHVwLXVwbG9hZHMvY21zLW1lZGlhLzEyNzYyOF81MjYzX1NDT19ORVBfZmMtV2ViLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJjb3ZlciIsInBvc2l0aW9uIjoidG9wIiwid2lkdGgiOjU4MCwiaGVpZ2h0IjoyNDB9LCJ3ZWJwIjp7InF1YWxpdHkiOjYwLCJsb3NzbGVzcyI6ZmFsc2V9fSwib3V0cHV0Rm9ybWF0Ijoid2VicCJ9 "
       },
       "f1": {

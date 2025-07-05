1043113059cdd8d7364177bfd4412acebb5e0b69
YoSin TV <61265398+yosintv@users.noreply.github.com>
2025-07-06 00:23:01 +0900

    Update ft.js

--- ft.js
+++ ft.js
@@ -2,7 +2,7 @@ const channels = {
     ____: " ____  ",
 
     clubb: " https://yosintv.github.io/yosintv/jwplayers.html?src=https://cheapmarket.blog/idea/?id=1 ",
-    chelsea: " https://yosintv.github.io/player/dmca.html?url=https://hls.uouetrq19.com/live/hd-en-6MtMdwaQr8ZBz1zxGP.m3u8?txSecret=d091cdb7f225911c1369cb4a294198be&txTime=6869CFBB ",
+    psg: " https://yosintv.github.io/player/1m.html?url=https://rzdtwfb81w5jihkz.buyapartments.online/v4/variant/VE1gTdz0mLzRnLv52bt9SMhFjdtM3ajFmc09iMxQGOwkTOihTZ1UWLmhzY40SYmNDNtMWN3cTLjJWOwIGMmNzL.m3u8 ",
 
 
     realmadrid: " https://yosintv.github.io/player/dmca.html?url=https://hls.uouetrq19.com/live/hd-en-6MtMdwaQr6Mi7YB37V.m3u8?txSecret=33e94531b4b020a6b21a5fb8b032080c&txTime=686581FB ",

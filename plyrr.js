64193f3a42a5231adde7120ad460678d9459ceb8
YoSin TV <61265398+yosintv@users.noreply.github.com>
2025-07-17 08:55:12 +0900

    Update plyrr.js

--- plyrr.js
+++ plyrr.js
@@ -2,18 +2,18 @@
         
       "mls": {
         url: " https://subrugopuciblchlvl6uqa666p23rig.happy-ending.site/54290fa6f10ebda2ecea576d1ae64ac3.m3u8 ",
-        poster: " https://images.fancode.com/skillup-uploads/cms-media/127628_5263_SCO_NEP_fc-Web.png?hash=eyJrZXkiOiJza2lsbHVwLXVwbG9hZHMvY21zLW1lZGlhLzEyNzYyOF81MjYzX1NDT19ORVBfZmMtV2ViLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJmaXQiOiJjb3ZlciIsInBvc2l0aW9uIjoidG9wIiwid2lkdGgiOjU4MCwiaGVpZ2h0IjoyNDB9LCJ3ZWJwIjp7InF1YWxpdHkiOjYwLCJsb3NzbGVzcyI6ZmFsc2V9fSwib3V0cHV0Rm9ybWF0Ijoid2VicCJ9 "
+        poster: " https://web.cricfoot.net/logo.png "
       },
 
         
       "f1": {
         url: " https://hls.uouetrq19.com/live/sd-6MtMPPaKE6nmMYz647.m3u8?txSecret=c7fb6b34c90e7b9580d20533cb6d021e&txTime=68798A0D ",
-        poster: "https://i.ibb.co/0RjkVHK0/IMG-20250614-213759-410.jpg"
+        poster: " https://web.cricfoot.net/logo.png "
       },
 
         
       "match2": {
         url: "https://1a-1791.com/live/t34ch9px/slot-35/euf9-qgmb_1080p/chunklist.m3u8",
-        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3jDvDkFrL6QvUm34ahcxcEIRQbMHYChERAA&s"
+        poster: " https://web.cricfoot.net/logo.png "
       }
     };
